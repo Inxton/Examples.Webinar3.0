71c492ebb59d0c5f2a1e4f112b6db8059228180d
PTKu <me@me.me>
2021-04-08 06:28:01 +0200

    TcOpen example project added

---
+++ TcOpen/XAETcOpen/Plc.Wpf/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Plc.Wpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App() : base()
+        {
+            PlcConnector.Entry.PlcController.Connector.BuildAndStart().ReadWriteCycleDelay = 50;
+        }
+    }
+}
---
+++ TcOpen/XAETcOpen/PlcConnector/Entry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlcConnector
+{
+    public static class Entry
+    {
+        public static Plc.PlcTwinController PlcController { get; } = new Plc.PlcTwinController(Vortex.Adapters.Connector.Tc3.Adapter.Tc3ConnectorAdapter.Create("172.20.10.2.1.1",851, true));
+    }
+}
